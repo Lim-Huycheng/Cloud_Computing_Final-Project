ae8fb24081960b35fc2621b701c6add6fbeb2df1
Keo-Sokanya <keosokanya18@gmail.com>
2025-07-15 13:50:09 +0700

    Load Balancer

--- terraform/main.tf
+++ terraform/main.tf
@@ -145,3 +145,40 @@ resource "aws_autoscaling_group" "web_asg" {
     create_before_destroy = true
   }
 }
+
+# --- Load Balancer ---
+resource "aws_lb" "web_lb" {
+  name               = "terraform-web-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.web_sg.id]
+  subnets            = data.aws_subnets.default.ids
+}
+
+resource "aws_lb_target_group" "web_tg" {
+  name     = "web-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "web_listener" {
+  load_balancer_arn = aws_lb.web_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_tg.arn
+  }
+}
