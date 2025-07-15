abffc836b400a1216795940b1c6fa4979c8f957e
KCLP1995 <chhaylymea43154001@gmail.com>
2025-07-15 13:25:52 +0700

    IAM

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,3 +38,40 @@ resource "aws_s3_bucket_policy" "public_policy" {
     }]
   })
 }
+
+# --- IAM Role for EC2 to read S3 assets ---
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_s3_read_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "ec2_s3_policy" {
+  name = "ec2_s3_read_policy"
+  role = aws_iam_role.ec2_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = ["s3:GetObject"],
+        Resource = "${aws_s3_bucket.website_assets.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2_instance_profile"
+  role = aws_iam_role.ec2_role.name
+}
