7dbaae8a556591702c83631ae54b1314191f9fd7
sugaringring <meaneatn@gmail.com>
2025-07-15 13:34:00 +0700

    Launch Template

--- terraform/main.tf
+++ terraform/main.tf
@@ -103,3 +103,20 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+# --- Launch Template with baked AMI (NO USER DATA) ---
+resource "aws_launch_template" "web_lt" {
+  name_prefix   = "web-template-"
+  image_id      = var.baked_ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ec2_profile.name
+  }
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups             = [aws_security_group.web_sg.id]
+  }
+}
