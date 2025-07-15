dcddb20dc08b281416f7e22312ee4f35183217ef
Chornay-coder <pichornayeng@gmail.com>
2025-07-15 13:24:10 +0700

    s3

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,3 +9,32 @@ data "aws_subnets" "default" {
     values = [data.aws_vpc.default.id]
   }
 }
+
+# --- S3 bucket for static assets only (images, CSS, JS) ---
+resource "aws_s3_bucket" "website_assets" {
+  bucket        = var.bucket_name
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_public_access_block" "block" {
+  bucket = aws_s3_bucket.website_assets.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "public_policy" {
+  bucket = aws_s3_bucket.website_assets.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Sid       = "PublicReadGetObject",
+      Effect    = "Allow",
+      Principal = "*",
+      Action    = "s3:GetObject",
+      Resource  = "${aws_s3_bucket.website_assets.arn}/*"
+    }]
+  })
+}
