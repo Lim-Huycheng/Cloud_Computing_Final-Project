9bb18c493db2615c59392ec701a75d9bab888ff7
Cheat123-CH <sokhcheatmet2024@gmail.com>
2025-07-15 13:36:29 +0700

    Auto-Scaling-Group

--- terraform/main.tf
+++ terraform/main.tf
@@ -120,3 +120,28 @@ resource "aws_launch_template" "web_lt" {
     security_groups             = [aws_security_group.web_sg.id]
   }
 }
+# --- Auto Scaling Group with EXACTLY 2 instances ---
+resource "aws_autoscaling_group" "web_asg" {
+  name                      = "web-asg"
+  desired_capacity          = 2
+  max_size                  = 2
+  min_size                  = 2
+  vpc_zone_identifier       = data.aws_subnets.default.ids
+  health_check_type         = "EC2"
+  target_group_arns         = [aws_lb_target_group.web_tg.arn]
+
+  launch_template {
+    id      = aws_launch_template.web_lt.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "TerraformWebServer"
+    propagate_at_launch = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
